2d0c5d73456d5dab3d83df1e0c36809b33c54baa
Donavan <donavan.aziaka.1@ulaval.ca>
2020-04-04 23:00:26 -0400

    feat : added database, api.js, login page and question page design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,8 @@ class Navbar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showDropdown: false
+            showDropdown: false,
+            showAccount: false
         };
     }
 
@@ -28,6 +29,14 @@ class Navbar extends React.Component {
 
     showDropdown() {
         if (this.state.showDropdown) {
+            this.setState({showAccount: false})
+        } else {
+            this.setState({showAccount: true})
+        }
+    }
+
+    showAccount() {
+        if (this.state.showAccount) {
             this.setState({showDropdown: false})
         } else {
             this.setState({showDropdown: true})
@@ -42,16 +51,19 @@ class Navbar extends React.Component {
                     <h2 className="navbar-routes-item" onClick={() => this.goToQuestion() }>New Question</h2>
                     <h2 className="navbar-routes-item" onClick={() => this.goToLeaderBoard() }>Leader Board</h2>
                 </div>
-                <div className="navbar-profile">
-                    <button className="dropbtn" onClick={() => this.showDropdown()}/>
-                    {
-                        this.state.showDropdown ?
-                        <div className="dropdown-content">
-                            <div className="user-profile">Hello User 1</div>
-                            <a href="/login" onClick={() => this.logout() }>Logout</a>
-                        </div> : ''
-                    }
-                </div>
+                {
+                    this.state.showAccount &&
+                    <div className="navbar-profile">
+                        <button className="dropbtn" onClick={() => this.showDropdown()}/>
+                        {
+                            this.state.showDropdown ?
+                                <div className="dropdown-content">
+                                    <div className="user-profile">Hello User 1</div>
+                                    <a href="/login" onClick={() => this.logout() }>Logout</a>
+                                </div> : ''
+                        }
+                    </div>
+                }
             </div>
         );
     }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../css/LoginPage.css"
 
 export default class LoginPage extends React.Component {
     constructor(props) {
@@ -6,10 +7,28 @@ export default class LoginPage extends React.Component {
         this.state = {};
     }
 
+    signIn() {
+        console.log("sign in")
+    }
+
+    createAccount() {
+        console.log("create account")
+    }
+
     render() {
         return (
-            <div>
-                LoginPage
+            <div className="login-page">
+                <div className="card">
+                    <div className="container">
+                        <h1>Sign in to your account</h1>
+                        <div>
+                            <label>Username</label>
+                            <input className="username" placeholder="Username"/>
+                        </div>
+                        <button onClick={() => this.signIn()} className="sign-in-button">Sign in</button>
+                        <span onClick={() => this.createAccount()} className="create-account">Create an account</span>
+                    </div>
+                </div>
             </div>
         );
     }
